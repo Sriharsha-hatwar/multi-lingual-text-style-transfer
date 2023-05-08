46988c032654c014c8f6f5184b54146d803c4fa5
Sriharsha-hatwar <shatwar@umass.edu>
2023-05-08 02:36:58 +0000

    Upadted objective 2

--- objective-2.py
+++ objective-2.py
@@ -15,77 +15,84 @@ wandb.login()
 
 metrics_pickle_data = []
 
-class SamanantarHindiTranslationDataset(Dataset):
-    def __init__(self, data: Dict[str, List[str]], tokenizer: MT5Tokenizer, source_max_length: int, target_max_length: int):
+class SamanantarHindiParaphraseDataset(Dataset):
+    def __init__(self, data: Dict[str, List[str]], tokenizer: MT5Tokenizer, x_max_length: int, x_para_max_length: int, paraphrase_lang: str):
         self.tokenizer = tokenizer
-        self.source_max_length = source_max_length
-        self.target_max_length = target_max_length
-        self.source_texts = data["src"]
-        self.target_texts = data["tgt"]
+        self.x_max_length = x_max_length
+        self.x_para_max_length = x_para_max_length
+        #print("Understanding source dataset : ", type(data["src"]))
+        #print("Understanding tgt dataset", len(data["src"]))
+        self.xs = data["tgt"] # one of the Indic languages
+        self.x_paras = data["paraphrase"]
         self.language_code = "hi "
     
     def __len__(self):
-        return len(self.source_texts)
+        return len(self.xs)
     
     def __getitem__(self, index):
-        source_text =  self.language_code + str(self.source_texts[index])
-        # Do we need to add in target text? - I dont think so.
-        target_text = str(self.target_texts[index])
+        x = '<cls>' + str(self.xs[index]) # '[CLS]'
+        #print(x)
+        x_para = '<cls>' + str(self.x_paras[index])
 
         # Tokenize the source and target texts
-        source_tokenized = self.tokenizer(source_text, padding="max_length", truncation=True, max_length=self.source_max_length, return_tensors="pt")
-        target_tokenized = self.tokenizer(target_text, padding="max_length", truncation=True, max_length=self.target_max_length, return_tensors="pt")
+        x_tokenized = self.tokenizer(x, padding="max_length", truncation=True, max_length=self.x_max_length, return_tensors="pt")
+        #print(x_tokenized)
+        x_para_tokenized = self.tokenizer(x_para, padding="max_length", truncation=True, max_length=self.x_para_max_length, return_tensors="pt")
 
-        source_ids = source_tokenized.input_ids.squeeze()
-        source_mask = source_tokenized.attention_mask.squeeze()
-        target_ids = target_tokenized.input_ids.squeeze()
-        target_mask = target_tokenized.attention_mask.squeeze()
+        #print(x_tokenized["input_ids"].shape)
+
+        #print(x_tokenized.input_ids.shape)
+        x_ids = x_tokenized.input_ids.squeeze()
+        x_mask = x_tokenized.attention_mask.squeeze()
+        x_para_ids = x_para_tokenized.input_ids.squeeze()
+        x_para_mask = x_para_tokenized.attention_mask.squeeze()
 
         return {
-            "input_ids": source_ids,
-            "attention_mask": source_mask,
-            "decoder_input_ids": target_ids, 
-            "decoder_attention_mask": target_mask,
-            "labels": target_ids,
-            "source_text" : source_text,
-            "target_text" : target_text
+            "x_ids": x_ids,
+            "x_masks": x_mask,
+            "x_para_ids": x_para_ids,
+            "x_para_masks": x_para_mask,
         }
-
-class SamanantarMarathiTranslationDataset(Dataset):
-    def __init__(self, data: Dict[str, List[str]], tokenizer: MT5Tokenizer, source_max_length: int, target_max_length: int):
+    
+class SamanantarMarathiParaphraseDataset(Dataset):
+    def __init__(self, data: Dict[str, List[str]], tokenizer: MT5Tokenizer, x_max_length: int, x_para_max_length: int, paraphrase_lang: str):
         self.tokenizer = tokenizer
-        self.source_max_length = source_max_length
-        self.target_max_length = target_max_length
-        self.source_texts = data["src"]
-        self.target_texts = data["tgt"]
+        self.x_max_length = x_max_length
+        self.x_para_max_length = x_para_max_length
+        #print("Understanding source dataset : ", type(data["src"]))
+        #print("Understanding tgt dataset", len(data["src"]))
+        self.xs = data["tgt"] # one of the Indic languages
+        self.x_paras = data["paraphrase"]
         self.language_code = "mr "
     
     def __len__(self):
-        return len(self.source_texts)
+        return len(self.xs)
     
     def __getitem__(self, index):
-        source_text =  self.language_code + str(self.source_texts[index])
-        # Do we need to add in target text? - I dont think so.
-        target_text = str(self.target_texts[index])
+        x = '<cls>' + str(self.xs[index]) # '[CLS]'
+        #print(x)
+        x_para = '<cls>' + str(self.x_paras[index])
 
         # Tokenize the source and target texts
-        source_tokenized = self.tokenizer(source_text, padding="max_length", truncation=True, max_length=self.source_max_length, return_tensors="pt")
-        target_tokenized = self.tokenizer(target_text, padding="max_length", truncation=True, max_length=self.target_max_length, return_tensors="pt")
+        x_tokenized = self.tokenizer(x, padding="max_length", truncation=True, max_length=self.x_max_length, return_tensors="pt")
+        #print(x_tokenized)
+        x_para_tokenized = self.tokenizer(x_para, padding="max_length", truncation=True, max_length=self.x_para_max_length, return_tensors="pt")
 
-        source_ids = source_tokenized.input_ids.squeeze()
-        source_mask = source_tokenized.attention_mask.squeeze()
-        target_ids = target_tokenized.input_ids.squeeze()
-        target_mask = target_tokenized.attention_mask.squeeze()
+        #print(x_tokenized["input_ids"].shape)
+
+        #print(x_tokenized.input_ids.shape)
+        x_ids = x_tokenized.input_ids.squeeze()
+        x_mask = x_tokenized.attention_mask.squeeze()
+        x_para_ids = x_para_tokenized.input_ids.squeeze()
+        x_para_mask = x_para_tokenized.attention_mask.squeeze()
 
         return {
-            "input_ids": source_ids,
-            "attention_mask": source_mask,
-            "decoder_input_ids": target_ids, 
-            "decoder_attention_mask": target_mask,
-            "labels": target_ids,
-            "source_text" : source_text,
-            "target_text" : target_text
+            "x_ids": x_ids,
+            "x_masks": x_mask,
+            "x_para_ids": x_para_ids,
+            "x_para_masks": x_para_mask,
         }
+
 
 class MT5ParaphraseModel(nn.Module):
     def __init__(self, model, device):
@@ -177,19 +184,21 @@ def train(model, batch_size, optimizer, train_loader, num_epochs, loss_criterion
     losses = AverageMeter()
     for index, batch in enumerate(tk0):
         optimizer.zero_grad()
-        batch_input_ids = batch['input_ids'].to(device)
-        batch_attention_mask = batch['attention_mask'].to(device)
-        batch_label_ids = batch['labels'].to(device)
+        batch_x_ids = batch['x_ids'].to(device)
+        batch_x_masks = batch['x_ids'].to(device)
+        batch_x_para_ids = batch['x_para_ids'].to(device)
+        batch_x_para_masks = batch['x_para_masks'].to(device)
         model.zero_grad()
-        outputs = model(batch_input_ids=batch_input_ids, batch_attention_mask=batch_attention_mask, batch_labels=batch_label_ids)
-        loss = outputs.loss
+        outputs = model(batch_x_ids, batch_x_para_ids)
+
+        loss = loss_criterion(model.pretrainedmodel.lm_head(outputs.last_hidden_state).view( -1, model.pretrainedmodel.lm_head(outputs.last_hidden_state).size(-1) ), batch_x_ids.view(-1) )
             
         loss.backward()
         optimizer.step()
 
         total_loss += loss.item() * batch_size
         processed_examples += batch_size
-        losses.update(loss.item(), batch_input_ids.shape[0])
+        losses.update(loss.item(), batch_x_ids.shape[0])
         tk0.set_postfix(loss=losses.avg)
     avg_loss = total_loss / processed_examples
     return avg_loss
@@ -203,14 +212,15 @@ def validation(model, batch_size, val_loader, loss_criterion, device):
     losses = AverageMeter()
     for index, batch in enumerate(tk0):
         with torch.no_grad():
-            batch_input_ids = batch['input_ids'].to(device)
-            batch_attention_mask = batch['attention_mask'].to(device)
-            batch_label_ids = batch['labels'].to(device)
-            outputs = model(batch_input_ids=batch_input_ids, batch_attention_mask=batch_attention_mask, batch_labels=batch_label_ids)
-            loss = outputs.loss
+            batch_x_ids = batch['x_ids'].to(device)
+            batch_x_masks = batch['x_ids'].to(device)
+            batch_x_para_ids = batch['x_para_ids'].to(device)
+            batch_x_para_masks = batch['x_para_masks'].to(device)
+            outputs = model(batch_x_ids, batch_x_para_ids)
+            loss = loss_criterion( model.pretrainedmodel.lm_head(outputs.last_hidden_state).view( -1, model.pretrainedmodel.lm_head(outputs.last_hidden_state).size(-1) ), batch_x_ids.view(-1) )
         total_loss += loss.item() * batch_size
         processed_examples += batch_size
-        losses.update(loss.item(), batch_input_ids.size(0))
+        losses.update(loss.item(), batch_x_ids.size(0))
         tk0.set_postfix(loss=losses.avg)
     avg_loss = total_loss/processed_examples
     return avg_loss
@@ -218,34 +228,34 @@ def validation(model, batch_size, val_loader, loss_criterion, device):
 def calculate_validation_perplexity(loss):
     return torch.exp(torch.tensor(loss))
 
-def calculate_validation_bleu(trained_model, val_loader):
-    bleu_score_summed = 0.0
-    no_of_batchs = 0
-    bleu_metric = SacreBLEUScore()
-    total_examples = 0
-    print("Calculating BLEU Score")
-    for batch in val_loader:
-        if total_examples < 200 : # Doing validation BLEU for only 200 samples...
-            no_of_batchs += 1
-            total_examples = total_examples + len(batch['target_text'])
-            batch_target = batch['target_text']
-            batch_input_ids = batch['input_ids'].to(device)
+# def calculate_validation_bleu(trained_model, val_loader):
+#     bleu_score_summed = 0.0
+#     no_of_batchs = 0
+#     bleu_metric = SacreBLEUScore()
+#     total_examples = 0
+#     print("Calculating BLEU Score")
+#     for batch in val_loader:
+#         if total_examples < 200 : # Doing validation BLEU for only 200 samples...
+#             no_of_batchs += 1
+#             total_examples = total_examples + len(batch['target_text'])
+#             batch_target = batch['target_text']
+#             batch_input_ids = batch['input_ids'].to(device)
 
-            batch_list_target = [[item] for item in batch_target] # Need to convert this way for bleu to work.
-            #inputs = tokenizer.batch_encode_plus(text, padding="max_length", truncation=True, max_length=128, return_tensors="pt").to(device)
-            outputs = trained_model.pretrainedmodel.generate(input_ids=batch_input_ids, max_new_tokens=40, do_sample=True, top_k=30, top_p=0.95)
-            generated_outputs = []
-            for i, output in enumerate(outputs):
-                generated_outputs.append(tokenizer.decode(outputs[i], skip_special_tokens=True))
+#             batch_list_target = [[item] for item in batch_target] # Need to convert this way for bleu to work.
+#             #inputs = tokenizer.batch_encode_plus(text, padding="max_length", truncation=True, max_length=128, return_tensors="pt").to(device)
+#             outputs = trained_model.pretrainedmodel.generate(input_ids=batch_input_ids, max_new_tokens=40, do_sample=True, top_k=30, top_p=0.95)
+#             generated_outputs = []
+#             for i, output in enumerate(outputs):
+#                 generated_outputs.append(tokenizer.decode(outputs[i], skip_special_tokens=True))
 
-            #bleu_metric = bleu_metric.compute(predictions = generated_outputs, references=batch_list_target)
-            #bleu_score_summed+=round(bleu_metric["score"], 1)
-            bleu_score = bleu_metric(generated_outputs, batch_list_target)
-            bleu_score_summed += bleu_score.item()
-        else:
-            break
-    print("BLEU Score calculation done.")
-    return bleu_score_summed / no_of_batchs
+#             #bleu_metric = bleu_metric.compute(predictions = generated_outputs, references=batch_list_target)
+#             #bleu_score_summed+=round(bleu_metric["score"], 1)
+#             bleu_score = bleu_metric(generated_outputs, batch_list_target)
+#             bleu_score_summed += bleu_score.item()
+#         else:
+#             break
+#     print("BLEU Score calculation done.")
+#     return bleu_score_summed / no_of_batchs
         
         
 def run(pretrained_model, device, train_dataset, val_dataset, num_epochs):
@@ -281,19 +291,21 @@ def run(pretrained_model, device, train_dataset, val_dataset, num_epochs):
 def load_dataset(lang, tokenizer):
     # Please remove the range in the below code.
     if lang == "hindi":
-        dataset = load_from_disk("./data/hindi_dataset_80k")
+        dataset = load_from_disk("./data/hindi_paraphrase_dataset_80k")
+        dataset = dataset["train"]
         dataset = dataset.train_test_split(test_size=0.2, seed=42)
         train_dataset = dataset["train"].shuffle(seed=42)
         val_dataset = dataset["test"].shuffle(seed=42)
-        train_dataset_final = SamanantarHindiTranslationDataset(train_dataset, tokenizer, 128, 128)
-        val_dataset_final = SamanantarHindiTranslationDataset(val_dataset, tokenizer, 128, 128)
+        train_dataset_final = SamanantarHindiParaphraseDataset(train_dataset, tokenizer, 128, 128)
+        val_dataset_final = SamanantarHindiParaphraseDataset(val_dataset, tokenizer, 128, 128)
     elif lang =="marathi":
-        dataset = load_from_disk("./data/marathi_dataset_20k")
+        dataset = load_from_disk("./data/marathi_paraphrase_dataset_20k")
+        dataset = dataset["train"]
         dataset = dataset.train_test_split(test_size=0.2, seed=42)
         train_dataset = dataset["train"].shuffle(seed=42)
         val_dataset = dataset["test"].shuffle(seed=42)
-        train_dataset_final = SamanantarMarathiTranslationDataset(train_dataset, tokenizer, 128, 128)
-        val_dataset_final = SamanantarMarathiTranslationDataset(val_dataset, tokenizer, 128, 128)
+        train_dataset_final = SamanantarMarathiParaphraseDataset(train_dataset, tokenizer, 128, 128)
+        val_dataset_final = SamanantarMarathiParaphraseDataset(val_dataset, tokenizer, 128, 128)
     else:
         print("Some issue, exiting.")
         exit(0)
@@ -329,12 +341,15 @@ if __name__ == "__main__":
 # Load the tokenizer and model
     tokenizer = MT5Tokenizer.from_pretrained("google/mt5-base")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    pretrained_model = MT5ForConditionalGeneration.from_pretrained("google/mt5-base").to(device)
+    pretrained_model = MT5ForConditionalGeneration.from_pretrainedMT5ForConditionalGeneration.from_pretrained('./checkpoints/objective_one/model_checkpointed').to(device)
+    special_tokens = {'additional_special_tokens': ['<cls>']}
+    num_added_toks = tokenizer.add_special_tokens(special_tokens)
+    pretrained_model.resize_token_embeddings(len(tokenizer))
     num_epochs = 20
     
     wandb.init(
         # Set the project where this run will be logged
-        project="objective1_cs685_project", 
+        project="objective2_cs685_project", 
         # We pass a run name (otherwise it’ll be randomly assigned, like sunshine-lollypop-10)
         name="run1", 
         # Track hyperparameters and run metadata
